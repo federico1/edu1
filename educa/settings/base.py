38676a8d95790a03da673b3300ddc1437a4109fe
federico1 <federico.roca@yahoo.com>
2018-08-26 00:10:37 -0400

    Update base.py

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -99,9 +99,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
