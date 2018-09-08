7fbbfa9021721e70ee8d622b0801030975b8ac5a
federico1 <federico.roca@yahoo.com>
2018-09-08 09:13:20 -0400

    Update urls.py

--- educa/urls.py
+++ educa/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+#from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from courses.views import CourseListView
 from django.conf import settings
