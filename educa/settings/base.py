19162b932b5d4a316511a847bd2147821e102784
federico1 <federico.roca@yahoo.com>
2018-09-15 10:14:43 -0400

    Update base.py

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__
 SECRET_KEY = 'xmo&z@u0(dfbq!w(dl_0a^f(s9ukugs%id3)i(ei=$oxg%ir80'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
