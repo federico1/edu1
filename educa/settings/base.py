69466f3a191e54cd58b51045b4a518e3335beeed
federico1 <federico.roca@yahoo.com>
2018-10-29 11:23:57 -0400

    Update base.py

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'students',
    # 'memcache_status',
-    'embed_video',
+    #'embed_video',
     #'rest_framework'
     #'rest_framework',
 )
